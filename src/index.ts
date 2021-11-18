f6bd0f7660ec9893a619a554216914bcebb6396e
ahadsenja <hamdanilm96@gmail.com>
2021-11-19 04:18:58 +0800

    chore: update index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello ts')
+import express, { Application, Request, Response } from 'express';
+
+import OperatorRoutes from './routers/operator/operator.routes';
+
+class App {
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+        this.routes();
+    }
+
+    routes(): void {
+        this.app.route('/').get((req: Request, res: Response) => {
+            res.send('This is test from library app');
+        });
+
+        this.app.use('/api/v1/operators', OperatorRoutes);
+    }
+}
+
+const port: number = 8000;
+const app = new App().app;
+
+app.listen(port, () => {
+    console.log('This app is running on port ' + `${port}`);
+});
