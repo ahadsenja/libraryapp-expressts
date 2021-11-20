3528c6c10c3bedcbc976e21c0caf96546e30be73
ahadsenja <hamdanilm96@gmail.com>
2021-11-20 16:43:54 +0800

    feat: add another endpoint and middleware

--- src/routers/operator/operator.routes.ts
+++ src/routers/operator/operator.routes.ts
@@ -1,10 +1,16 @@
 import BaseRoutes from '../base.routes';
+
 import OperatorController from '../../controllers/operator.controller';
+import { auth } from '../../middleware/auth.middleware';
 
 class OperatorRoutes extends BaseRoutes {
 
     public routes(): void {
-        this.router.get('/', OperatorController.getAll);
+        this.router.get('/', auth, OperatorController.getAll);
+        this.router.get('/:id', auth, OperatorController.getById);
+        this.router.post('/', auth, OperatorController.create);
+        this.router.put('/:id', auth, OperatorController.update);
+        this.router.delete('/:id', auth, OperatorController.delete);
     }
 }
 
