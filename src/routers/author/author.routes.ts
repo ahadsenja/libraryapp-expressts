4a13ec29a3969c7039663f6bdef5efcc823d8b2f
ahadsenja <hamdanilm96@gmail.com>
2021-11-22 14:21:10 +0800

    feat: create author routes

---
+++ src/routers/author/author.routes.ts
@@ -0,0 +1,15 @@
+import authorController from "../../controllers/author/author.controller";
+import { auth } from "../../middleware/auth.middleware";
+import BaseRoutes from "../base.routes";
+
+class AuthorRoutes extends BaseRoutes {
+    public routes(): void {
+        this.router.get('/', auth, authorController.findAll);
+        this.router.get('/:id', auth, authorController.findOne);
+        this.router.post('/', auth, authorController.create);
+        this.router.put('/:id', auth, authorController.update);
+        this.router.delete('/:id', auth, authorController.delete);
+    }
+}
+
+export default new AuthorRoutes().router;
