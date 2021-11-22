f75fd948cbc14a310e35808b4b0d94a4889af692
ahadsenja <hamdanilm96@gmail.com>
2021-11-22 14:20:05 +0800

    feat: create author controller

---
+++ src/controllers/author/author.controller.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from "express";
+
+import IController from "../../interface/controller.interface";
+const db = require('../../db/models');
+
+class AuthorController implements IController {
+    findAll = async (req: Request, res: Response): Promise<Response> => {
+        const authors = await db.author.findAll();
+        return res.send({
+            data: authors
+        });
+    }
+
+    findOne = async (req: Request, res: Response): Promise<Response> => {
+        const { id } = req.params;
+
+        const author = await db.author.findOne({
+            where: { id }
+        });
+
+        return res.send({
+            data: author
+        });
+    }
+
+    create = async (req: Request, res: Response): Promise<Response> => {
+        const { name, email, address, born_date, born_place, city } = req.body;
+
+        const author = await db.author.create({
+            name, email, address, born_date, born_place, city
+        });
+
+        return res.send({
+            data: author
+        });
+    }
+
+    update = async (req: Request, res: Response): Promise<Response> => {
+        const { id } = req.params;
+        const { name, email, address, born_date, born_place, city } = req.body;
+
+        const author = await db.author.update({
+            name, email, address, born_date, born_place, city
+        },
+            {
+                where: { id }
+            });
+
+        return res.send({
+            data: author
+        })
+    }
+
+    delete = async (req: Request, res: Response): Promise<Response> => {
+        const { id } = req.params;
+
+        const author = await db.author.destroy({
+            where: { id }
+        });
+
+        return res.send({
+            data: author
+        });
+    }
+
+}
+
+export default new AuthorController();
