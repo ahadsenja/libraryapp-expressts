0d6b23f1c90384baa1b6265dbf84ba3dd56cf00d
ahadsenja <hamdanilm96@gmail.com>
2021-11-22 10:33:29 +0800

    chore: update findAll and findOne method

--- src/interface/controller.interface.ts
+++ src/interface/controller.interface.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 
 interface IController {
-    getAll(req: Request, res: Response): Response | Promise<Response>;
-    getById(req: Request, res: Response): Response | Promise<Response>;
+    findAll(req: Request, res: Response): Response | Promise<Response>;
+    findOne(req: Request, res: Response): Response | Promise<Response>;
     create(req: Request, res: Response): Response | Promise<Response>;
     update(req: Request, res: Response): Response | Promise<Response>;
     delete(req: Request, res: Response): Response | Promise<Response>;
