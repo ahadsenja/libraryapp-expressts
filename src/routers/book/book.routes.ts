f47cf2b5db071b274bf28cd52ff889e87d7a148f
ahadsenja <hamdanilm96@gmail.com>
2021-11-22 14:00:32 +0800

    feat: create book routes

---
+++ src/routers/book/book.routes.ts
@@ -0,0 +1,15 @@
+import BookController from "../../controllers/book/book.controller";
+import { auth } from "../../middleware/auth.middleware";
+import BaseRoutes from "../base.routes";
+
+class BookRoutes extends BaseRoutes {
+    public routes(): void {
+        this.router.get('/', auth, BookController.findAll);
+        this.router.get('/:id', auth, BookController.findOne);
+        this.router.post('/', auth, BookController.create);
+        this.router.put('/:id', auth, BookController.update);
+        this.router.delete('/:id', auth, BookController.delete);
+    }
+}
+
+export default new BookRoutes().router;
