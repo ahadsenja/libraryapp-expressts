6d2b8c48e04a567fbd430d528a80a5390818cc66
ahadsenja <hamdanilm96@gmail.com>
2021-11-22 10:31:42 +0800

    chore: update crud controller

--- src/controllers/operator.controller.ts
+++ src/controllers/operator.controller.ts
@@ -4,7 +4,7 @@ const db = require('../db/models');
 import IController from '../interface/controller.interface';
 
 class OperatorController implements IController {
-    getAll = async (req: Request, res: Response): Promise<Response> => {
+    findAll = async (req: Request, res: Response): Promise<Response> => {
         const operator = await db.operator.findAll();
         return res.send({
             data: operator,
@@ -12,20 +12,59 @@ class OperatorController implements IController {
         });
     }
 
-    getById = async (req: Request, res: Response): Promise<Response> => {
-        throw new Error('Method not implemented.');
+    findOne = async (req: Request, res: Response): Promise<Response> => {
+        const { id } = req.params;
+
+        const operator = await db.operator.findOne({
+            where: { id },
+            attributes: ['id', 'name', 'username', 'email', 'password', 'handphone', 'address']
+        });
+
+        return res.send({
+            data: operator,
+            message: 'Successfully get data by id'
+        })
     }
 
     create = async (req: Request, res: Response): Promise<Response> => {
-        throw new Error('Method not implemented.');
+        const { name, username, email, password, handphone, address } = req.body;
+
+        const operator = await db.operator.create({
+            name, username, email, password, handphone, address
+        });
+
+        return res.send({
+            data: operator,
+            message: 'Successfully created data'
+        });
     }
 
     update = async (req: Request, res: Response): Promise<Response> => {
-        throw new Error('Method not implemented.');
+        const { id } = req.params;
+        const { name, username, email, password, handphone, address } = req.body;
+
+        const operator = await db.operator.update({
+            name, username, email, password, handphone, address
+        }, {
+            where: { id }
+        });
+
+        return res.send({
+            data: operator,
+            message: 'Data successfully updated'
+        })
     }
 
     delete = async (req: Request, res: Response): Promise<Response> => {
-        throw new Error('Method not implemented.');
+        const { id } = req.params;
+
+        const operator = await db.operator.destroy({
+            where: { id }
+        });
+
+        return res.send({
+            message: 'Data successfully deleted'
+        })
     }
 }
 
