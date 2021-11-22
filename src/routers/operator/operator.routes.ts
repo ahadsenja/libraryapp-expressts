7b32d75128512ab464eb5548a8f5f96660871c4f
ahadsenja <hamdanilm96@gmail.com>
2021-11-22 10:34:06 +0800

    chore: update router get

--- src/routers/operator/operator.routes.ts
+++ src/routers/operator/operator.routes.ts
@@ -6,8 +6,8 @@ import { auth } from '../../middleware/auth.middleware';
 class OperatorRoutes extends BaseRoutes {
 
     public routes(): void {
-        this.router.get('/', auth, OperatorController.getAll);
-        this.router.get('/:id', auth, OperatorController.getById);
+        this.router.get('/', auth, OperatorController.findAll);
+        this.router.get('/:id', auth, OperatorController.findOne);
         this.router.post('/', auth, OperatorController.create);
         this.router.put('/:id', auth, OperatorController.update);
         this.router.delete('/:id', auth, OperatorController.delete);
