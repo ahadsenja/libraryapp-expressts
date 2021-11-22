deebe9c942b01a543004236b34426acb4578042b
ahadsenja <hamdanilm96@gmail.com>
2021-11-22 12:45:36 +0800

    feat: create customer router

---
+++ src/routers/customer/customer.routes.ts
@@ -0,0 +1,15 @@
+import customerController from "../../controllers/customer/customer.controller";
+import { auth } from "../../middleware/auth.middleware";
+import BaseRoutes from "../base.routes";
+
+class CustomerRouter extends BaseRoutes {
+    public routes(): void {
+        this.router.get('/', auth, customerController.findAll);
+        this.router.get('/:id', auth, customerController.findOne);
+        this.router.post('/', auth, customerController.create);
+        this.router.put('/:id', auth, customerController.update);
+        this.router.delete('/:id', auth, customerController.delete);
+    }
+}
+
+export default new CustomerRouter().router;
