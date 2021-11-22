719df651975b2318f6c407dc83e1d18589175590
ahadsenja <hamdanilm96@gmail.com>
2021-11-22 14:39:56 +0800

    feat: create genre routes

---
+++ src/routers/genre/genre.routes.ts
@@ -0,0 +1,15 @@
+import { auth } from "../../middleware/auth.middleware";
+import GenreController from "../../controllers/genre/genre.controller";
+import BaseRoutes from "../base.routes";
+
+class GenreRoutes extends BaseRoutes {
+    public routes(): void {
+        this.router.get('/', auth, GenreController.findAll);
+        this.router.get('/:id', auth, GenreController.findOne);
+        this.router.post('/', auth, GenreController.create);
+        this.router.put('/:id', auth, GenreController.update);
+        this.router.delete('/:id', auth, GenreController.delete);
+    }
+}
+
+export default new GenreRoutes().router;
