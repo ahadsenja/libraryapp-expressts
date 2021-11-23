3a63f3afe1de8b1d143e09d55b0f8ddb90829908
ahadsenja <hamdanilm96@gmail.com>
2021-11-23 09:27:43 +0800

    feat: create publisher routes

---
+++ src/routers/publisher/publisher.routes.ts
@@ -0,0 +1,15 @@
+import PublisherController from "../../controllers/publisher/publisher.controller";
+import { auth } from "../../middleware/auth.middleware";
+import BaseRoutes from "../base.routes";
+
+class PublisherRoutes extends BaseRoutes {
+    public routes(): void {
+        this.router.get('/', auth, PublisherController.findAll);
+        this.router.get('/:id', auth, PublisherController.findOne);
+        this.router.post('/', auth, PublisherController.create);
+        this.router.put('/:id', auth, PublisherController.update);
+        this.router.delete('/:id', auth, PublisherController.delete);
+    }
+}
+
+export default new PublisherRoutes().router;
