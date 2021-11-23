d4d436988354b8703170640ae6c1ccdb31657b1c
ahadsenja <hamdanilm96@gmail.com>
2021-11-23 09:49:08 +0800

    feat: create new setting

---
+++ src/controllers/setting/setting.controller.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+
+import IController from "../../interface/controller.interface";
+const db = require('../../db/models');
+
+class SettingController implements IController {
+    findAll = async (req: Request, res: Response): Promise<Response> => {
+        const setting = await db.setting.findAll();
+
+        return res.send({
+            data: setting
+        });
+    }
+
+    findOne = async (req: Request, res: Response): Promise<Response> => {
+        const { id } = req.params;
+
+        const setting = await db.setting.findOne({
+            where: { id }
+        });
+
+        return res.send({
+            data: setting
+        });
+    }
+
+    create = async (req: Request, res: Response): Promise<Response> => {
+        const { property, value } = req.body;
+
+        const setting = await db.setting.create({
+            property, value
+        });
+
+        return res.send({
+            data: setting
+        });
+    }
+
+    update = async (req: Request, res: Response): Promise<Response> => {
+        const { id } = req.params;
+        const { property, value } = req.body;
+
+        const setting = await db.setting.update({
+            property, value
+        }, {
+            where: { id }
+        });
+
+        return res.send({
+            data: setting
+        });
+    }
+
+    delete = async (req: Request, res: Response): Promise<Response> => {
+        const { id } = req.params;
+
+        const setting = await db.setting.destroy({
+            where: { id }
+        });
+
+        return res.send({
+            data: setting
+        });
+    }
+}
+
+export default new SettingController();
