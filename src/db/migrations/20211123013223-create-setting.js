1d4dec4f35b7db296d0d1535d5914910174f034b
ahadsenja <hamdanilm96@gmail.com>
2021-11-23 09:33:22 +0800

    feat: create setting migration

---
+++ src/db/migrations/20211123013223-create-setting.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('settings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      property: {
+        type: Sequelize.STRING
+      },
+      value: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('settings');
+  }
+};
