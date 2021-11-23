e639a7c5270fb2da6da0a9b5494e2dfd27246e6c
ahadsenja <hamdanilm96@gmail.com>
2021-11-23 09:49:52 +0800

    feat: create setting routes

---
+++ src/routers/setting/setting.routes.ts
@@ -0,0 +1,15 @@
+import SettingController from "../../controllers/setting/setting.controller";
+import { auth } from "../../middleware/auth.middleware";
+import BaseRoutes from "../base.routes";
+
+class SettingRoutes extends BaseRoutes {
+    public routes(): void {
+        this.router.get('/', auth, SettingController.findAll);
+        this.router.get('/:id', auth, SettingController.findOne);
+        this.router.post('/', auth, SettingController.create);
+        this.router.put('/:id', auth, SettingController.update);
+        this.router.delete('/:id', auth, SettingController.delete);
+    }
+}
+
+export default new SettingRoutes().router;
