925ec252aa64c49238309ae4fb957e8f89ed5c42
ahadsenja <hamdanilm96@gmail.com>
2021-11-30 13:33:37 +0800

    feat: create authentication routes

---
+++ src/routers/auth/auth.routes.ts
@@ -0,0 +1,14 @@
+import AuthController from "../../controllers/auth/auth.controller";
+import { auth } from "../../middlewares/auth.middleware";
+import BaseRoutes from "../base.routes";
+import validate from '../../middlewares/auth.validator';
+
+class AuthRoutes extends BaseRoutes {
+    public routes(): void {
+        this.router.post('/register', validate, AuthController.register);
+        this.router.post('/login', validate, AuthController.login);
+        this.router.get('/profile', auth, AuthController.profile);
+    }
+}
+
+export default new AuthRoutes().router;
