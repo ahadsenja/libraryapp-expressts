084759536c8f0cddea565d434f048eb317d96067
ahadsenja <hamdanilm96@gmail.com>
2021-11-30 08:11:32 +0800

    feat: create borrow routes

---
+++ src/routers/borrow/borrow.routes.ts
@@ -0,0 +1,15 @@
+import BorrowController from "../../controllers/borrow/borrow.controller";
+import { auth } from "../../middleware/auth.middleware";
+import BaseRoutes from "../base.routes";
+
+class BorrowRoutes extends BaseRoutes {
+    public routes(): void {
+        this.router.get('/', auth, BorrowController.findAll);
+        this.router.get('/:id', auth, BorrowController.findOne);
+        this.router.post('/', auth, BorrowController.create);
+        this.router.put('/:id', auth, BorrowController.update);
+        this.router.delete('/:id', auth, BorrowController.delete);
+    }
+}
+
+export default new BorrowRoutes().router;
