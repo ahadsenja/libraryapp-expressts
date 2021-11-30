184a3559e3502e81104cb0cd28056d8c5b47b0be
ahadsenja <hamdanilm96@gmail.com>
2021-11-30 13:32:49 +0800

    feat: create authentication middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const auth = (req: Request, res: Response, next: NextFunction): any => {
-    console.log('this is middleware');
-    next();
+    if (!req.headers.authorization) {
+        return res.status(401).send('No token found');
+    }
+
+    let secretKey = process.env.JWT_SECRET_KEY || 'secretkey';
+    const token: string = req.headers.authorization.split(" ")[1];
+
+    try {
+        const credential: string | object = jwt.verify(token, secretKey);
+
+        if (credential) {
+            req.app.locals.credential = credential;
+            next();
+        }
+
+        return res.send('Invalid token');
+    } catch (error) {
+        return res.send(error);
+    }
 }
