993358b1883dba4b30de0b0d85a09efa7171bba4
ahadsenja <hamdanilm96@gmail.com>
2021-11-30 13:34:25 +0800

    feat: create authentication function for hashing password, generate token and comparing password

---
+++ src/utils/Authentication.ts
@@ -0,0 +1,21 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+class Authentication {
+    public static passwordHash = (password: string): Promise<string> => {
+        return bcrypt.hash(password, 10);
+    }
+
+    public static passwordCompare = async (text: string, encryptedText: string): Promise<boolean> => {
+        let result = await bcrypt.compare(text, encryptedText);
+        return result;
+    }
+
+    public static generateToken = (id: number, email: string, password: string): string => {
+        const secretKey: string = process.env.JWT_SECRET_KEY || 'secretkey';
+        const token: string = jwt.sign({ id, email, password }, secretKey);
+        return token;
+    }
+}
+
+export default Authentication;
