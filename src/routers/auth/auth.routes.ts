baf15430a2c62039eff657c00fecede88037b44b
ahadsenja <hamdanilm96@gmail.com>
2022-01-20 16:35:34 +0800

    chore: add google auth url

--- src/routers/auth/auth.routes.ts
+++ src/routers/auth/auth.routes.ts
@@ -2,13 +2,18 @@ import AuthController from "../../controllers/auth/auth.controller";
 import { auth } from "../../middlewares/auth.middleware";
 import BaseRoutes from "../base.routes";
 import validate from '../../middlewares/auth.validator';
+import passport from "passport";
 
 class AuthRoutes extends BaseRoutes {
     public routes(): void {
         this.router.post('/register', validate, AuthController.register);
         this.router.post('/login', validate, AuthController.login);
+        this.router.post('/google', AuthController.loginWithGoogle);
         this.router.get('/profile', auth, AuthController.profile);
-        // this.router.get('/google', auth, AuthController.oAuth)
+
+        // this.router.post('/google', auth, validate, passport.authenticate(
+        //     'google-token', { session: false }
+        // ), AuthController.loginWithGoogle);
     }
 }
 
