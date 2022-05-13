f9dae94e55ea9c95ffa3bb3043bd9724c60a471b
ahadsenja <hamdanilm96@gmail.com>
2022-05-13 08:19:11 +0800

    chore: add timezone

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,8 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "timezone": "Asia/Makassar",
   },
   "test": {
     "username": "root",
@@ -20,6 +21,7 @@ module.exports = {
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "timezone": "Asia/Makassar",
   }
 }
